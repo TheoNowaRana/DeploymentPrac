3019d284bc6f802c48c43510ffc769ee2f245230
TheoNowaRana <143871852+TheoNowaRana@users.noreply.github.com>
2025-01-16 21:24:15 +0800

    Update App.js
    yes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Ready To Deploy Guys! WebDev For the Winn</p>
-        <a className="App-link" href="https://react.dev/learn" target="_blank" rel="noreferrer noopener">Learn React</a>
       </header>
     </div>
   );
